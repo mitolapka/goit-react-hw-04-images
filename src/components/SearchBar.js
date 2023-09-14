665a62ad780603571a788cdc86693c8589d13e9c
mitolapka <124922415+mitolapka@users.noreply.github.com>
2023-09-14 13:59:54 +0300

    change

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,74 +1,34 @@
-import React, { Component } from 'react';
+// Searchbar.js
+import React, { useState } from 'react';
 
-export class Searchbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: '',
-    };
-  }
+export const Searchbar = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.props.onSearch(this.state.query);
+    onSearch(query);
   };
 
-  handleInputChange = (e) => {
-    this.setState({ query: e.target.value });
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
   };
 
-  render() {
-    return (
-      <header className="Searchbar">
-        <form className="SearchForm" onSubmit={this.handleSubmit}>
-          <button type="submit" className="SearchForm-button">
-            <span className="SearchForm-button-label">Search</span>
-          </button>
-          <input
-            className="SearchForm-input"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.query}
-            onChange={this.handleInputChange}
-          />
-        </form>
-      </header>
-    );
-  }
-}
-
-
-
-// import React, { useState } from 'react';
-
-// export const Searchbar = ({ onSearch }) => {
-//     const [query, setQuery] = useState('');
-
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//     onSearch(query);
-//   };
-
-//   return (
-//     <header className="Searchbar">
-//       <form className="SearchForm" onSubmit={handleSubmit}>
-//         <button type="submit" className="SearchForm-button">
-//           <span className="SearchForm-button-label">Search</span>
-//         </button>
-//         <input
-//           className="SearchForm-input"
-//           type="text"
-//           autoComplete="off"
-//           autoFocus
-//           placeholder="Search images and photos"
-//           value={query}
-//           onChange={(e) => setQuery(e.target.value)}
-//         />
-//       </form>
-//     </header>
-//   );
-// }
-
-
+  return (
+    <header className="Searchbar">
+      <form className="SearchForm" onSubmit={handleSubmit}>
+        <button type="submit" className="SearchForm-button">
+          <span className="SearchForm-button-label">Search</span>
+        </button>
+        <input
+          className="SearchForm-input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={query}
+          onChange={handleInputChange}
+        />
+      </form>
+    </header>
+  );
+};
